03c241c79d33e14cdf63a0ce7b0e74d02cda12ea
anikait <anikaitshivam@yahoo.in>
2015-05-19 18:44:20 +0530

    First commit

---
+++ src/main/java/olympus/services/test/Main.java
@@ -0,0 +1,10 @@
+package olympus.services.test;
+
+/**
+ * Created by anikait on 19/5/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
